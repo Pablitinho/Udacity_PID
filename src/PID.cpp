a80daa752667e6fa844acd74d6a5129a71c10293
pablitinho <pablogsanchez@gmail.com>
2018-04-22 09:39:48 +0200

    First implementation of the PID

--- src/PID.cpp
+++ src/PID.cpp
@@ -10,12 +10,36 @@ PID::PID() {}
 
 PID::~PID() {}
 
-void PID::Init(double Kp, double Ki, double Kd) {
+void PID::Init(double Kp_, double Ki_, double Kd_)
+{
+	Kp=Kp_;
+	Ki=Ki_;
+	Kd=Kd_;
+	prev_cte=0.0;
+	cte_i=0.0;
 }
 
-void PID::UpdateError(double cte) {
+void PID::UpdateError(double cte)
+{
+	p_error = -Kp*cte;
+	i_error = -Ki*cte_i;
+	d_error = -Kd*(cte-prev_cte);
+
+	prev_cte = cte;
+	cte_i += cte;
 }
 
-double PID::TotalError() {
+double PID::TotalError()
+{
+	double te=p_error+i_error+d_error;
+	if (te<-1)
+	{
+		te=-1;
+	}
+	else if(te>1)
+	{
+		te=1;
+	}
+	return te;
 }
 
--- src/PID.h
+++ src/PID.h
@@ -9,6 +9,8 @@ public:
   double p_error;
   double i_error;
   double d_error;
+  double prev_cte;
+  double cte_i;
 
   /*
   * Coefficients
@@ -30,7 +32,7 @@ public:
   /*
   * Initialize PID.
   */
-  void Init(double Kp, double Ki, double Kd);
+  void Init(double Kp_, double Ki_, double Kd_);
 
   /*
   * Update the PID error variables given cross track error.
